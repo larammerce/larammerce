d42dd030478f891937bf93ef7ed9fbfda11a7672
Arash Khajelou <a.khajelou@gmail.com>
2023-01-07 11:24:21 +0330

    Fix locales typo in tranlation config.

--- config/translation.php
+++ config/translation.php
@@ -97,7 +97,7 @@ return [
     | The rtl locales that change the direction of website design.
     |
     */
-    "rtl_locates" => ["fa", "ar"],
+    "rtl_locales" => ["fa", "ar"],
 
     /*
     |--------------------------------------------------------------------------
