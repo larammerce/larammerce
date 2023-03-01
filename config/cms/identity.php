c42f94e0ef1a8965ac3f37d970a38819e52d9895
Arash Khajelou <a.khajelou@gmail.com>
2023-03-01 13:15:03 +0330

    Revert "Remove Hinza Identity."
    This reverts commit ed4962c8194087c4e2fcdbd269206c5c0bce66a2.

--- config/cms/identity.php
+++ config/cms/identity.php
@@ -12,5 +12,15 @@ return [
         "url" => "https://larammerce.com",
         "color" => "#ff2e20",
         "fav" => "favicon"
+    ],
+    "hinza" => [
+        "logo" => "/admin_dashboard/images/logos/hinza",
+        "title" => "سامانه مدیریت وبسایت هینزا",
+        "name" => "Hinza Ecommerce Cms",
+        "motto" => "سیستــــم مدیریـت محتوای هینــــــزا",
+        "website" => "www.hinzaco.com",
+        "url" => "https://hinzaco.com",
+        "color" => "#69ff47",
+        "fav" => "favicon/hinza"
     ]
 ];
