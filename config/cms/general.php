458195542fdd25bbbade56439de4c2fce0973ce1
Arash Khajelou <a.khajelou@gmail.com>
2023-05-22 18:29:38 +0330

    Make the only_main_model views optional.

--- config/cms/general.php
+++ config/cms/general.php
@@ -3,6 +3,7 @@
 return [
     "site" => [
         "model_options" => env('SITE_MODEL_OPTIONS', false),
+        "only_main_models" => env('SITE_ONLY_MAIN_COLORS_VIEW', true),
         "show_deactivated_products" => env('SITE_SHOW_DEACTIVATED_PRODUCTS', false),
         "enable_directory_location" => env('SITE_ENABLE_DIRECTORY_LOCATION', false),
         "product_sort" => env("SITE_DEFAULT_PRODUCT_SORT", "created_at:desc"),
