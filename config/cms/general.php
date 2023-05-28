581545c0aecbc7865de0bd20920ead9909159748
Arash Khajelou <a.khajelou@gmail.com>
2023-05-28 23:55:05 +0330

    Make the default value for `enable_directory_location` false

--- config/cms/general.php
+++ config/cms/general.php
@@ -5,7 +5,7 @@ return [
         "model_options" => env('SITE_MODEL_OPTIONS', false),
         "only_main_models" => env('SITE_ONLY_MAIN_COLORS_VIEW', true),
         "show_deactivated_products" => env('SITE_SHOW_DEACTIVATED_PRODUCTS', false),
-        "enable_directory_location" => env('SITE_ENABLE_DIRECTORY_LOCATION', true),
+        "enable_directory_location" => env('SITE_ENABLE_DIRECTORY_LOCATION', false),
         "product_sort" => env("SITE_DEFAULT_PRODUCT_SORT", "created_at:desc"),
         "stock_manager_notification" => env("SITE_STOCK_MANAGER_NOTIFICATION", true)
     ]
