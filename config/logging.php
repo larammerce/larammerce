24fa6c4f0dd2f993789c5a2341b5d474803a783f
Arash Khajelou <a.khajelou@gmail.com>
2023-08-05 12:16:00 +0330

    Add sentry log channel.

--- config/logging.php
+++ config/logging.php
@@ -50,8 +50,16 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'sentry'],
             'ignore_exceptions' => false,
+        ],
+
+        'sentry' => [
+            'driver' => 'sentry',
+            // The minimum logging level at which this handler will be triggered
+            // Available levels: debug, info, notice, warning, error, critical, alert, emergency
+            'level' => env('LOG_LEVEL', 'error'),
+            'bubble' => true, // Whether the messages that are handled can bubble up the stack or not
         ],
 
         'single' => [
