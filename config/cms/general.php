65ad1f02d07569981bc6416c12f9e31321f85fba
Arash Khajelou <a.khajelou@gmail.com>
2023-08-11 20:25:56 +0330

    Update the migration.

--- config/cms/general.php
+++ config/cms/general.php
@@ -7,6 +7,6 @@ return [
         "show_deactivated_products" => env('SITE_SHOW_DEACTIVATED_PRODUCTS', false),
         "enable_directory_location" => env('SITE_ENABLE_DIRECTORY_LOCATION', false),
         "product_sort" => env("SITE_DEFAULT_PRODUCT_SORT", "created_at:desc"),
-        "stock_manager_notification" => env("SITE_STOCK_MANAGER_NOTIFICATION", true)
+        "stock_manager_notification" => env("SITE_STOCK_MANAGER_NOTIFICATION", false)
     ]
 ];
