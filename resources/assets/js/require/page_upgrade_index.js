7d8b4667465ec930aa524492547cabc7e558f239
Arash Khajelou <a.khajelou@gmail.com>
2023-08-21 20:22:00 +0330

    Enhance the upgrade process.

--- resources/assets/js/require/page_upgrade_index.js
+++ resources/assets/js/require/page_upgrade_index.js
@@ -2,34 +2,77 @@ if (window.PAGE_ID === "admin.pages.upgrade.index")
     require(["jquery"], function (jQuery) {
 
         let upgradeInProgress = false;
+        const logLines = [];
+
+        function toggleLoading() {
+            if (upgradeInProgress) {
+                jQuery("button, a, input").addClass("disabled").attr("disabled", "disabled").attr("loading", "loading");
+                jQuery("#updating-note").fadeIn();
+            } else {
+                jQuery("[loading=loading]").removeClass("disabled").removeAttr("disabled").removeAttr("loading");
+                jQuery("#updating-note").fadeOut();
+            }
+        }
 
         function handleUpgrade(url) {
             upgradeInProgress = true;
-            const source = new EventSource(url);
+            toggleLoading();
 
-            jQuery("button, a, input").addClass("disabled").attr("disabled", "disabled").attr("loading", "loading");
-            jQuery("#updating-note").fadeIn();
+            jQuery.ajax({
+                url: url,
+                type: 'GET',
+                dataType: 'json'
+            }).done(function (data) {
+                let gettingLog = false;
+                const upgradeInterval = setInterval(function () {
+                    if (gettingLog) {
+                        document.getElementById("output").innerHTML += " .";
+                        return;
+                    }
 
-            source.onmessage = function(event) {
-                // Close the connection if the server sends a specific end signal.
-                if (event.data.startsWith('END:')) {
-                    source.close();
-                    upgradeInProgress = false;
-                    jQuery("#updating-note").fadeOut();
-                    jQuery("[loading=loading]").removeClass("disabled").removeAttr("disabled").removeAttr("loading");
-                    document.getElementById("output").innerHTML += "Process finished working.<br>";
-                }else{
-                    document.getElementById("output").innerHTML += event.data + "<br>";
-                }
-            };
+                    gettingLog = true;
+                    jQuery.ajax({
+                        url: '/admin/upgrade-log',
+                        data: {
+                            "line_number": logLines.length
+                        },
+                        type: 'GET',
+                        dataType: 'json'
+                    }).done(function (data) {
+                        gettingLog = false;
+                        if (data.log.length > 0) {
+                            // split the data.log with \n
+                            const lines = data.log.split("\n");
+                            // remove the last line, because it's empty
+                            lines.pop();
 
-            source.onerror = function(event) {
-                source.close();
-                console.error("EventSource failed:", event);
-            };
+                            // add the lines to the logLines array
+                            logLines.push(...lines);
+
+                            // add the lines to the output div
+                            document.getElementById("output").innerHTML += "<br>" + lines.join("<br>");
+
+                            // see if the data.running is false stop the interval
+                            if (!data.running) {
+                                clearInterval(upgradeInterval);
+                                upgradeInProgress = false;
+                                toggleLoading();
+                            }
+                        }
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        gettingLog = false;
+                        console.log()
+                    });
+                }, 2000);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                upgradeInProgress = false;
+                toggleLoading();
+
+                document.getElementById("output").innerHTML += "Another upgrade in progress! Try again in a minute!";
+            });
         }
 
-        window.onbeforeunload = function() {
+        window.onbeforeunload = function () {
             if (upgradeInProgress) {
                 return "Upgrade in progress. Are you sure you want to leave this page?";
             }
