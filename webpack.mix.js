6c433ff5c9aaabbe2374a6edf6505cc80a738599
Arash Khajelou <a.khajelou@gmail.com>
2023-10-30 18:52:52 +0330

    Remove the sample comment.

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,7 +8,6 @@ const mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- | Add some sample comment.
  |
  */
 
