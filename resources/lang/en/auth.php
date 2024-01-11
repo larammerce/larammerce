a25a96503e4847b8eb6ce4e54051ec39cc5a513c
kamran saeidi <152760867+KamranSaeidi@users.noreply.github.com>
2024-01-11 20:56:33 +0330

    Update auth.php
    modify ' instead "

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,6 +15,6 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.', 
-    'not_logged_in" => "Please login to your account first.',
-    'forbidden" => "Unfortunately, you do not have access to this section.'
+    'not_logged_in' => 'Please login to your account first.',
+    'forbidden' => 'Unfortunately, you do not have access to this section.'
 ];
