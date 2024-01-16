cf082fb0cebbcf104a327df680eab064c05753da
Arash Khajelou <a.khajelou@gmail.com>
2024-01-17 02:20:17 +0330

    Add en locale.

---
+++ resources/lang/en/sms.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    "drivers" => [
+        "file" => "ذخیره در فایل (عدم ارسال)",
+        "farapayamak" => "سیستم پیام کوتاه فراپیامک",
+        "kavenegar" => "سرویس پیام کوتاه کاوه‌نگار",
+    ],
+
+    "config" => [
+        "is_enabled" => "فعالسازی",
+        "username" => "نام کاربری",
+        "password" => "رمز عبور",
+        "token" => "توکن",
+        "host" => "آدرس هاست",
+        "port" => "پورت",
+        "number" => "شماره",
+        "flash_support" => "نمایش پاپ آپ و عدم ذخیره در دستگاه گیرنده",
+        "can_send_sms_for_invoice_submit" => "ارسال پیامک برای ثبت سفارش",
+        "can_send_sms_for_invoice_paid" => "ارسال پیامک برای پرداخت شده",
+        "can_send_sms_for_invoice_cancel" => "ارسال پیامک برای لغو سفارش",
+        "can_send_sms_for_invoice_sending" => "ارسال پیامک برای در حال ارسال",
+        "can_send_sms_for_invoice_sent" => "ارسال پیامک برای ارسال شده",
+        "can_send_sms_for_invoice_delivered" => "ارسال پیامک برای تحویل شده",
+        "can_send_sms_for_invoice_survey" => "ارسال پیامک برای نظرسنجی",
+    ]
+];
